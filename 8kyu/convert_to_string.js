cb27caaba93fe0464601f5c5463d0132eac0a3a2
Héctor González Pereda <hgonza02@ucm.es>
2021-06-26 21:48:11 +0200

    convert to string

--- 8kyu/convert_to_string.js
+++
@@ -1,4 +0,0 @@
-//We need a function that can transform a number into a string.
-function convert_to_string(num){
-
-}
---
+++ js/1_convert_to_string.js
@@ -0,0 +1,10 @@
+//We need a function that can transform a number into a string.
+
+console.log(convert_to_string(5));
+
+function convert_to_string(num){
+    return num.toString(); 
+    
+    // convierte un número en una cadena de texto
+    // toString();
+}
